1acb19b3c76baf93c40609ebeb6295d3c49e73dd
mattfurness <matt.furness@gmail.com>
2018-08-31 14:36:39 +1000

    Week 06 Homework

--- src/Week06/Fibonacci.hs
+++ src/Week06/Fibonacci.hs
@@ -16,62 +16,88 @@ module Week06.Fibonacci
   , fib4
   ) where
 
+import Data.List (unfoldr)
+
 ---------------------------  Exercise 1
 
 fib :: Integer -> Integer
-fib =
-  error "Week06.Fibonacci#fib not implemented"
+fib 0 = 0
+fib 1 = 1
+fib x = fib (x - 1) + fib (x - 2)
+
 
 fibs1 :: [Integer]
-fibs1 =
-  error "Week06.Fibonacci#fibs1 not implemented"
+fibs1 = map fib [0..]
 
 --------------------------- Exercise 2
 
 fibs2 :: [Integer]
-fibs2 =
-  error "Week06.Fibonacci#fibs2 not implemented"
+fibs2 = unfoldr (Just . nextFib) (0, 1)
+  where
+    nextFib a = (fst a, (snd a, uncurry (+) a))
 
 --------------------------- Exercise 3
 
-data Stream a = ImplementMe
+data Stream a = a :+ Stream a
 
 streamToList :: Stream a -> [a]
-streamToList =
-  error "Week06.Fibonacci#streamToList not implemented"
+streamToList (a :+ as) = a : streamToList as
+
+infixr 5 :+
 
 --------------------------- Exercise 4
 
 streamRepeat :: a -> Stream a
-streamRepeat =
-  error "Week06.Fibonacci#streamRepeat not implemented"
+streamRepeat = (:+) <*> streamRepeat
 
 streamMap :: (a -> b) -> Stream a -> Stream b
-streamMap =
-  error "Week06.Fibonacci#streamMap not implemented"
+streamMap f (a :+ as) = f a :+ streamMap f as
 
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed =
-  error "Week06.Fibonacci#streamFromSeed not implemented"
+streamFromSeed f a = a :+ streamFromSeed f (f a)
+
+streamInterleave :: Stream a -> Stream a -> Stream a
+streamInterleave (x :+ xs) ys = x :+ streamInterleave ys xs
 
 --------------------------- Exercise 5
 
 nats :: Stream Integer
-nats =
-  error "Week06.Fibonacci#nats not implemented"
+nats = streamFromSeed (+ 1) 0
 
 ruler :: Stream Integer
-ruler =
-  error "Week06.Fibonacci#ruler not implemented"
+ruler = interleaveIncrement $ streamRepeat 0
+  where
+    repeatIncrement = streamRepeat . (+ 1)
+    interleaveIncrement (x :+ xs) = streamInterleave xs (interleaveIncrement $ repeatIncrement x)
 
 --------------------------- Exercise 6
 
+x :: Stream Integer
+x = 0 :+ 1 :+ streamRepeat 0
+
+instance Num (Stream Integer) where
+  fromInteger = (:+ streamRepeat 0)
+  negate = streamMap negate
+  (+) (a :+ as) (b :+ bs) = a + b :+ as + bs
+  (*) (a0 :+ a') b@(b0 :+ b') = a0 * b0 :+ streamMap (* a0) b' + a' * b
+
+instance Fractional (Stream Integer) where
+  (/) a@(a0 :+ a') b@(b0 :+ b') = (a0 `div` b0) :+ streamMap (`div` b0) (a' - a / b * b')
+
 fibs3 :: Stream Integer
-fibs3 =
-  error "Week06.Fibonacci#fibs3 not implemented"
+fibs3 = x / (1 - x - x ^ 2)
 
 --------------------------- Exercise 7
 
+data Matrix = Matrix Integer Integer Integer Integer
+
+instance Num Matrix where
+  (*) (Matrix a0 b0 c0 d0) (Matrix a1 b1 c1 d1) =
+    Matrix (a0 * a1 + b0 * c1) (a0 * b1 + b0 * d1) (c0 * a1 + d0 * c1) (c0 * b1 + d0 * d1)
+
 fib4 :: Integer -> Integer
-fib4 =
-  error "Week06.Fibonacci#fibs4 not implemented"
+fib4 0 = 0
+fib4 n = (pluckFib . toPower) n
+  where
+    toPower = (Matrix 1 1 1 0 ^)
+    pluckFib (Matrix _ fib _ _) = fib
