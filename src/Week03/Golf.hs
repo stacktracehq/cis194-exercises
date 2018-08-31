bafab60a3a436a49d788d5c27b7e9ab84f2d78f4
mattfurness <matt.furness@gmail.com>
2018-08-31 14:35:03 +1000

    Week 03 Homework

--- src/Week03/Golf.hs
+++ src/Week03/Golf.hs
@@ -4,11 +4,35 @@ module Week03.Golf
   , histogram
   ) where
 
+import Data.List (sort, group, transpose)
+import Data.Char (isSpace)
+
+snd3 :: (a, b, c) -> b
+snd3 (_, b, _) = b
+
 skips :: [a] -> [[a]]
-skips = error "Week03.Golf#skips not implemented"
+skips l =  zipWith takeEvenyNth [1..] $ replicate (length l) l
+  where
+    isDivisibleBy n = (0==) . (`mod` n)
+    takeEvenyNth n = map snd . filter (isDivisibleBy n . fst) . zip [1..]
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima = error "Week03.Golf#localMaxima not implemented"
+localMaxima l = map snd3 $ filter isSndMax $ zip3 l (drop 1 l) (drop 2 l)
+  where
+    isSndMax (a, b, c) = (a < b) && (b > c)
 
 histogram :: [Integer] -> String
-histogram = error "Week03.Golf#histogram not implemented"
+histogram
+  = unlines
+  . dropWhile (all isSpace)
+  . reverse
+  . transpose
+  . footer
+  . map (makeColumn . drop 1)
+  . group
+  . sort
+  . (++ [0..9])
+  where
+    strPadRight a b cs = cs ++ replicate (b - length cs) a
+    makeColumn = strPadRight ' ' 10 . map (const '*')
+    footer = zipWith (\a b -> show a ++ "=" ++ b) [0..]
