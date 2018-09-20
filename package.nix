a0d0070c5cbf3cad994e3f52c8bfcced6b724608
Brad Parker <hi@bradparker.com>
2018-09-20 14:46:42 +1000

    Add hedgehog, exciting

--- package.nix
+++ package.nix
@@ -1,10 +1,14 @@
-{ mkDerivation, base, containers, hspec, mtl, stdenv }:
+{ mkDerivation, base, containers, hedgehog, hspec
+, hw-hspec-hedgehog, mtl, stdenv
+}:
 mkDerivation {
   pname = "stacktrace-cis194";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [ base containers mtl ];
-  testHaskellDepends = [ base containers hspec ];
+  testHaskellDepends = [
+    base containers hedgehog hspec hw-hspec-hedgehog
+  ];
   homepage = "https://github.com/stacktracehq/cosmos/tree/master/training/CIS194";
   description = "Resources for working through CIS194 together";
   license = stdenv.lib.licenses.bsd3;
