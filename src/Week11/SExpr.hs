0027563800f36eb8e3df4fb3140cf04a6384021e
Brad Parker <hi@bradparker.com>
2018-10-05 10:10:46 +1000

    Week 11

--- src/Week11/SExpr.hs
+++ src/Week11/SExpr.hs
@@ -1,5 +1,6 @@
 module Week11.SExpr where
 
+import Prelude hiding (lex)
 import Control.Applicative
 import Data.Char (isSpace, isAlpha, isAlphaNum)
 import Week11.AParser (Parser, satisfy, char, posInt)
@@ -8,19 +9,19 @@ import Week11.AParser (Parser, satisfy, char, posInt)
 --  Exercise 1: Parsing repetitions
 ------------------------------------------------------------
 zeroOrMore :: Parser a -> Parser [a]
-zeroOrMore = error "Week11.SExpr#zeroOrMore not implemented"
+zeroOrMore p = oneOrMore p <|> pure[]
 
 oneOrMore :: Parser a -> Parser [a]
-oneOrMore = error "Week11.SExpr#oneOrMore not implemented"
+oneOrMore p = (:) <$> p <*> zeroOrMore p
 
 ------------------------------------------------------------
 --  Exercise 2: Utilities
 ------------------------------------------------------------
 spaces :: Parser String
-spaces = error "Week11.SExpr#spaces not implemented"
+spaces = zeroOrMore (satisfy isSpace)
 
 ident :: Parser String
-ident = error "Week11.SExpr#ident not implemented"
+ident = (:) <$> satisfy isAlpha <*> zeroOrMore (satisfy isAlphaNum)
 
 ------------------------------------------------------------
 --  Exercise 3: Parsing S-expressions
@@ -42,5 +43,14 @@ data SExpr
   | Comb [SExpr]
   deriving (Show, Eq)
 
+lex :: Parser a -> Parser a
+lex p = p <* spaces
+
+parseAtom :: Parser Atom
+parseAtom = lex (N <$> posInt <|> I <$> ident)
+
+parseComb :: Parser [SExpr]
+parseComb = lex (char '(') *> oneOrMore (lex parseSExpr) <* lex (char ')')
+
 parseSExpr :: Parser SExpr
-parseSExpr = error "Week11.SExpr#parseSExpr not implemented"
+parseSExpr = spaces *> (A <$> parseAtom <|> Comb <$> parseComb)
