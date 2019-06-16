17cebe0e2e99ef7fc0e35efb9e4ae459508c863b
Ovindu Manawaduge <odmanawaduge@gmail.com>
2019-06-16 12:57:30 +1000

    week 2 solutions

--- src/Week02/LogAnalysis.hs
+++ src/Week02/LogAnalysis.hs
@@ -17,20 +17,51 @@ import Week02.Log
   , testParse
   )
 
+isInt :: String -> Bool
+isInt s = not $ null (reads s :: [(Int, String)])
+
+parseInt :: String -> Int
+parseInt = read
+
+parseMessageParts :: [String] -> LogMessage
+parseMessageParts ("I":time:rest) | isInt time = 
+  LogMessage Info (parseInt time) (unwords rest) 
+parseMessageParts ("W":time:rest) | isInt time = 
+  LogMessage Warning (parseInt time) (unwords rest) 
+parseMessageParts ("E":level:time:rest) | isInt level && isInt time = 
+  LogMessage (Error (parseInt level)) (parseInt time) (unwords rest) 
+parseMessageParts x = Unknown (unwords x) 
+
 parseMessage :: String -> LogMessage
-parseMessage = error "Week02.LogAnalysis#parseMessage not implemented"
+parseMessage = parseMessageParts . words
 
 parse :: String -> [LogMessage]
-parse = error "Week02.LogAnalysis#parse not implemented"
+parse = (map parseMessage) . lines
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = error "Week02.LogAnalysis#insert not implemented"
+insert (Unknown _) tree = tree
+insert msg Leaf = Node Leaf msg Leaf
+insert msg@(LogMessage _ msgTime _) (Node lT nodeMsg@(LogMessage _ nodeTime _) rT) 
+  | msgTime < nodeTime = Node (insert msg lT) nodeMsg rT
+  | otherwise          = Node lT nodeMsg (insert msg rT)
+insert _ (Node _ (Unknown _ ) _) = error "Unexpected MessageTree state"
 
 build :: [LogMessage] -> MessageTree
-build = error "Week02.LogAnalysis#build not implemented"
+build = foldl (flip $ insert) Leaf
 
 inOrder :: MessageTree -> [LogMessage]
-inOrder = error "Week02.LogAnalysis#inOrder not implemented"
+inOrder Leaf = []
+inOrder (Node lT msg rT) = (inOrder lT) ++ [msg] ++ (inOrder rT) 
 
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong = error "Week02.LogAnalysis#whatWentWrong not implemented"
+whatWentWrong = (map getMessage) . (filter isError) . inOrder . build 
+  where 
+    isError :: LogMessage -> Bool
+    isError (LogMessage (Error s) _ _)
+      | s >= 50 = True
+      | otherwise = False
+    isError _ = False
+
+    getMessage :: LogMessage -> String
+    getMessage (LogMessage _ _ msg) = msg
+    getMessage (Unknown _) = error "Unexpected LogMessage state"
