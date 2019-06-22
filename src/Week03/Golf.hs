d60e8c1448ea0fe6d67173ba2752143120612bef
Ovindu Manawaduge <odmanawaduge@gmail.com>
2019-06-23 01:10:19 +1000

    week 3 solutions

--- src/Week03/Golf.hs
+++ src/Week03/Golf.hs
@@ -4,11 +4,35 @@ module Week03.Golf
   , histogram
   ) where
 
+-- skipBy :: [a] -> Int -> [a]
+-- skipBy as s = case (drop s as) of 
+--   [] -> []
+--   (x:xs) -> x : skipBy xs s
+
+skipBy :: [a] -> Int -> [a]
+skipBy as s = map (snd) $ filter (\n -> (fst n) `mod` s == 0) $ zip [1..length as] as
+
 skips :: [a] -> [[a]]
-skips = error "Week03.Golf#skips not implemented"
+skips as = map (skipBy as) [1..length as]
+
+-- localMaxima :: [Integer] -> [Integer]
+-- localMaxima xs@(a:b:c:_)
+--   | b > a && b > c = b : localMaxima (drop 1 xs)
+--   | otherwise = localMaxima (drop 1 xs)
+-- localMaxima _ = []
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima = error "Week03.Golf#localMaxima not implemented"
+localMaxima xs = map (\(_,b,_) -> b) $ filter (\(a,b,c) -> b > a && b > c) $ zip3 xs (drop 1 xs) (drop 2 xs)
+
+counts :: [Integer] -> [Int]
+counts xs = [count x xs | x <- [0..9]] where
+  count :: Integer -> [Integer] -> Int
+  count v = length . filter (== v) 
+
+drawHist :: [Int] -> String
+drawHist xs = unlines $ reverse $ map (drawHistLine xs) [1..(maximum xs)] where
+  drawHistLine :: [Int] -> Int -> String
+  drawHistLine cs l = [if c >= l then '*' else ' ' | c <- cs]
 
 histogram :: [Integer] -> String
-histogram = error "Week03.Golf#histogram not implemented"
+histogram xs = (drawHist $ counts xs) ++ "==========\n0123456789\n"
