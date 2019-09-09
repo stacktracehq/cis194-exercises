7a7ba4e123bd7351828d98ff1bf4e21fe1026d13
Matt Furness <matthew.furness@blackbaud.me>
2019-09-09 10:03:57 +1000

    Use brittany instead of hindent

--- nix/tools.nix
+++ nix/tools.nix
@@ -2,7 +2,7 @@ packages:
   [
     packages.direnv
     packages.haskellPackages.cabal-install
-    packages.haskellPackages.hindent
     packages.ghcid
     packages.haskellPackages.hlint
+    packages.haskellPackages.brittany
   ]
