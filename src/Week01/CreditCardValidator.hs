f77b0f5e884b965d136490c7b9d83e25c5dcba82
Ben Tefay <ben.tefay@stacktracehq.com>
2019-09-13 17:21:14 +1000

    Complete part of week 1

--- src/Week01/CreditCardValidator.hs
+++ src/Week01/CreditCardValidator.hs
@@ -7,11 +7,22 @@ module Week01.CreditCardValidator
   )
 where
 
+-- reverse' :: [a] -> [a]
+-- reverse' []       = []
+-- reverse' (x : xs) = reverse' xs ++ [x]
+
+reverse'' :: [a] -> [a]
+reverse'' = foldl (flip (:)) []
+
+divMod' :: Integer -> Integer -> (Integer, Integer)
+divMod' n r = (n `div` r, n `mod` r)
+
 toDigits :: Integer -> [Integer]
-toDigits = error "Week01.CreditCardValidator#toDigits not implemented"
+toDigits = reverse'' . toDigitsRev
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = error "Week01.CreditCardValidator#toDigitsRev not implemented"
+toDigitsRev x | x <= 0    = []
+              | otherwise = let (d, m) = divMod' x 10 in m : toDigitsRev d
 
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther =
