d701be54dda1cae920ec6577ae715a2d1fa62cba
Nick Young <nick.young@stacktracehq.com>
2019-09-13 16:56:20 +1000

    Credit Card Validator

--- src/Week01/CreditCardValidator.hs
+++ src/Week01/CreditCardValidator.hs
@@ -7,18 +7,26 @@ module Week01.CreditCardValidator
   )
 where
 
+
 toDigits :: Integer -> [Integer]
-toDigits = error "Week01.CreditCardValidator#toDigits not implemented"
+toDigits = reverse . toDigitsRev
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = error "Week01.CreditCardValidator#toDigitsRev not implemented"
+toDigitsRev a
+  | a <= 0 = []
+  | otherwise = a `mod` 10 : toDigitsRev (a `div` 10)
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther =
-  error "Week01.CreditCardValidator#doubleEveryOther not implemented"
+doubleEveryOther xs = 
+  let
+    accumulator x (True, list) = (False, (2*x) : list)
+    accumulator x (False, list) = (True, x : list)
+    (_, result) = foldr accumulator (False, []) xs
+  in result
 
 sumDigits :: [Integer] -> Integer
-sumDigits = error "Week01.CreditCardValidator#sumDigits not implemented"
+sumDigits = sum . (toDigits =<<)
 
 validate :: Integer -> Bool
-validate = error "Week01.CreditCardValidator#validate not implemented"
+validate x = mod (sumDigits (doubleEveryOther (toDigits x))) 10 == 0
+
