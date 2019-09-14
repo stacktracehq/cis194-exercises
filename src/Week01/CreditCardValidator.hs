31c90047ca6c4b0fd7f1f2ba0f6ea863a8d4bc4e
mattfurness <matt.furness@gmail.com>
2019-09-15 09:38:51 +1000

    Week01

--- src/Week01/CreditCardValidator.hs
+++ src/Week01/CreditCardValidator.hs
@@ -1,24 +1,37 @@
 module Week01.CreditCardValidator
   ( toDigits
   , toDigitsRev
+  , doubleEveryOther'
   , doubleEveryOther
   , sumDigits
   , validate
   )
 where
 
+import           Data.List                      ( unfoldr )
+
 toDigits :: Integer -> [Integer]
-toDigits = error "Week01.CreditCardValidator#toDigits not implemented"
+toDigits = reverse . toDigitsRev
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = error "Week01.CreditCardValidator#toDigitsRev not implemented"
+toDigitsRev n =
+  let f b | b <= 0    = Nothing
+          | otherwise = Just (b `mod` 10, b `div` 10)
+  in  unfoldr f n
+
+doubleEveryOther' :: [Integer] -> [Integer]
+doubleEveryOther' =
+  let doubleFromStart = zipWith ($) (cycle [id, (* 2)])
+  in  reverse . doubleFromStart . reverse
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther =
-  error "Week01.CreditCardValidator#doubleEveryOther not implemented"
+doubleEveryOther ns =
+  let getFn pred' = if pred' then (2 *) else id
+      foldFn n (pred', ns') = (not pred', getFn pred' n : ns')
+  in  snd $ foldr foldFn (False, []) ns
 
 sumDigits :: [Integer] -> Integer
-sumDigits = error "Week01.CreditCardValidator#sumDigits not implemented"
+sumDigits = sum . (toDigits =<<)
 
 validate :: Integer -> Bool
-validate = error "Week01.CreditCardValidator#validate not implemented"
+validate = (== 0) . (`rem` 10) . sumDigits . doubleEveryOther . toDigits
