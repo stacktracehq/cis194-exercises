cff257dccb4d69fdcde9cbd0cd98c41cbefe4a74
mattfurness <matt.furness@gmail.com>
2019-09-15 11:05:00 +1000

    Add megaparsec

--- package.nix
+++ package.nix
@@ -1,13 +1,13 @@
 { mkDerivation, base, containers, hedgehog, hspec
-, hw-hspec-hedgehog, MonadRandom, mtl, stdenv, lens
+, hw-hspec-hedgehog, MonadRandom, mtl, stdenv, lens, megaparsec
 }:
 mkDerivation {
   pname = "stacktrace-cis194";
   version = "0.1.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base containers MonadRandom mtl ];
+  libraryHaskellDepends = [ base containers MonadRandom mtl lens megaparsec ];
   testHaskellDepends = [
-    base containers hedgehog hspec hw-hspec-hedgehog lens
+    base containers hedgehog hspec hw-hspec-hedgehog
   ];
   homepage = "https://github.com/stacktracehq/cosmos/tree/master/training/CIS194";
   description = "Resources for working through CIS194 together";
