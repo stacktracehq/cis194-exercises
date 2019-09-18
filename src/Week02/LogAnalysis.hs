cc38f62a8e5ea76a38139f60aa31bbd7c0a2315b
Brad Bow <brad.bow@stacktracehq.com>
2019-09-19 08:37:38 +1000

    Week02 solns

--- src/Week02/LogAnalysis.hs
+++ src/Week02/LogAnalysis.hs
@@ -18,19 +18,50 @@ import           Week02.Log                     ( LogMessage(..)
                                                 )
 
 parseMessage :: String -> LogMessage
-parseMessage = error "Week02.LogAnalysis#parseMessage not implemented"
+parseMessage line = case words line of
+  "I"     : t : msg -> LogMessage Info (read t) (unwords msg)
+  "W"     : t : msg -> LogMessage Warning (read t) (unwords msg)
+  "E" : l : t : msg -> LogMessage (Error (read l)) (read t) (unwords msg)
+  _                 -> Unknown line
 
 parse :: String -> [LogMessage]
-parse = error "Week02.LogAnalysis#parse not implemented"
+parse = map parseMessage . lines
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = error "Week02.LogAnalysis#insert not implemented"
+insert (Unknown _) tree                       = tree
+insert lm          Leaf                       = Node Leaf lm Leaf
+insert lm          (Node lt u@(Unknown _) rt) = Node lt u (insert lm rt)
+insert lm@(LogMessage _ t1 _) (Node lt y@(LogMessage _ t2 _) rt)
+  | t1 <= t2  = Node (insert lm lt) y rt
+  | otherwise = Node lt y (insert lm rt)
+
+-- build :: [LogMessage] -> MessageTree
+-- build = foldl (flip insert) Leaf
 
 build :: [LogMessage] -> MessageTree
-build = error "Week02.LogAnalysis#build not implemented"
+build []      = Leaf
+build (h : t) = go t (insert h Leaf)
+ where
+  go :: [LogMessage] -> MessageTree -> MessageTree
+  go []       tree = tree
+  go (x : xs) tree = go xs (insert x tree)
 
 inOrder :: MessageTree -> [LogMessage]
-inOrder = error "Week02.LogAnalysis#inOrder not implemented"
+inOrder Leaf            = []
+inOrder (Node lt lm rt) = inOrder lt ++ [lm] ++ inOrder rt
 
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong = error "Week02.LogAnalysis#whatWentWrong not implemented"
+-- whatWentWrong ls = map getMessage (inOrder (build (severeErrors ls)))
+whatWentWrong = (getMessage <$>) . inOrder . build . severeErrors
+ where
+  severeErrors :: [LogMessage] -> [LogMessage]
+  severeErrors logs = filter isSevereError logs
+
+  isSevereError :: LogMessage -> Bool
+  isSevereError (LogMessage (Error l) _ _) = l > 50
+  isSevereError _                          = False
+
+  getMessage :: LogMessage -> String
+  getMessage (LogMessage _ _ m) = m
+  getMessage (Unknown m       ) = m
+
