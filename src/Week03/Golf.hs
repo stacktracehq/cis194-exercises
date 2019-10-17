ed2e11c75f3152264c18c8b3c767000f5bb30e9b
Brad Bow <brad.bow@stacktracehq.com>
2019-10-18 09:39:32 +1000

    Week03 solns

--- src/Week03/Golf.hs
+++ src/Week03/Golf.hs
@@ -6,10 +6,23 @@ module Week03.Golf
 where
 
 skips :: [a] -> [[a]]
-skips = error "Week03.Golf#skips not implemented"
+skips xs = map (`everyNth` xs) [1 .. length xs]
+  where everyNth n = map snd . filter ((== 0) . (`mod` n) . fst) . zip [1 ..]
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima = error "Week03.Golf#localMaxima not implemented"
+localMaxima =
+  let triples xs = zip3 xs (drop 1 xs) (drop 2 xs)
+  in  map (\(_, n, _) -> n) . filter (\(a, n, b) -> n > a && n > b) . triples
 
 histogram :: [Integer] -> String
-histogram = error "Week03.Golf#histogram not implemented"
+histogram xs =
+  let buildRows fs = map (\i -> buildRow i fs) (reverse [1 .. (maximum fs)])
+      buildRow i = map (\f -> if f >= i then '*' else ' ')
+      frequencies = map (\i -> (frequencyOf i xs)) [0 .. 9]
+      frequencyOf n = fromIntegral . length . filter (== n)
+
+      rows      = buildRows frequencies
+      separator = replicate 10 '='
+      legend    = concatMap show [0 .. 9]
+  in  unlines $ rows ++ [separator, legend]
+
