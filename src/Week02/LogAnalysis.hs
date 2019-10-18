04bc4dad3a63c10c8c7d49acbac719cfbd456b6e
Fionna Song <fionna.song@stacktracehq.com>
2019-10-18 16:57:34 +1000

    Week02

--- src/Week02/LogAnalysis.hs
+++ src/Week02/LogAnalysis.hs
@@ -17,20 +17,48 @@ import           Week02.Log                     ( LogMessage(..)
                                                 , testParse
                                                 )
 
+
 parseMessage :: String -> LogMessage
-parseMessage = error "Week02.LogAnalysis#parseMessage not implemented"
+parseMessage message = createMessage (words message)
+  where
+    createMessage :: [String] -> LogMessage
+    createMessage ("I":t:c) = LogMessage (Info) (read t) (unwords c)
+    createMessage ("W":t:c) = LogMessage (Warning) (read t) (unwords c)
+    createMessage ("E":s:t:c) = LogMessage (Error(read s)) (read t) (unwords c)
+    createMessage msg =  Unknown (unwords msg)
+
+
 
 parse :: String -> [LogMessage]
-parse = error "Week02.LogAnalysis#parse not implemented"
+parse messages = map parseMessage (lines messages) 
+
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = error "Week02.LogAnalysis#insert not implemented"
+insert (Unknown _) tree = tree
+insert target Leaf =  Node Leaf target Leaf 
+insert target@(LogMessage _ targetTime _) (Node left source@(LogMessage _ sourceTime _) right)
+  | targetTime < sourceTime = Node (insert target left) source right
+  | otherwise = Node left source (insert target right)
+insert _ tree = tree
+
 
 build :: [LogMessage] -> MessageTree
-build = error "Week02.LogAnalysis#build not implemented"
+build msgList = foldr insert Leaf (reverse msgList)
 
 inOrder :: MessageTree -> [LogMessage]
-inOrder = error "Week02.LogAnalysis#inOrder not implemented"
+inOrder (Node Leaf msg Leaf) = [msg]
+inOrder (Node left msg right) = concat [inOrder(left), [msg], inOrder(right)]
+inOrder _  = []
 
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong = error "Week02.LogAnalysis#whatWentWrong not implemented"
+whatWentWrong items = map giveMeContent (filterMe (inOrder (build items)))
+
+   where 
+    giveMeContent :: LogMessage -> String
+    giveMeContent (LogMessage _ _ content) = content
+    giveMeContent (Unknown content) = content
+
+    filterMe :: [LogMessage] -> [LogMessage]
+    filterMe input  = filter (\item -> case item of 
+      (LogMessage (Error errorNum) _ _) ->  errorNum >= 50 
+      _ -> False) input
