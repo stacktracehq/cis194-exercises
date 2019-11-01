de50800899f5aedeada262817a3655fd16901d93
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2019-11-01 16:31:09 +1000

    Week 03

--- src/Week03/Golf.hs
+++ src/Week03/Golf.hs
@@ -5,11 +5,73 @@ module Week03.Golf
   )
 where
 
+import Data.Bifunctor (second)
+import Data.List (transpose)
+
 skips :: [a] -> [[a]]
-skips = error "Week03.Golf#skips not implemented"
+skips = 
+  let
+    replicateItAll :: [a] -> [[a]]
+    replicateItAll as = replicate (length as) as
+
+    filterItAll :: (Int, [a]) -> [a]
+    filterItAll (i, word) = filterWithIndex ((== 0) . (`mod` i) . fst) word
+
+    zipWithIndex :: [a] -> [(Int, a)]
+    zipWithIndex = zip [1..]
+
+    mapWithIndex :: ((Int, a) -> b) -> [a] -> [b]
+    mapWithIndex f = map f . zipWithIndex
+
+    filterWithIndex :: ((Int, a) -> Bool) -> [a] -> [a]
+    filterWithIndex f = map snd . filter f . zipWithIndex
+  in
+    mapWithIndex filterItAll . replicateItAll
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima = error "Week03.Golf#localMaxima not implemented"
+localMaxima = 
+  let
+    zipBeforeAndAfter :: a -> [a] -> [(a, a, a)]
+    zipBeforeAndAfter a xs = zip3 (drop 1 xs) xs (a : xs)
+
+    isLocalMaxima :: (Integer, Integer, Integer) -> Bool
+    isLocalMaxima (a, b, c) = b > a && b > c
+
+    getMiddle :: (Integer, Integer, Integer) -> Integer
+    getMiddle (_, x, _) = x
+  in
+    map getMiddle . filter isLocalMaxima . zipBeforeAndAfter 0
 
 histogram :: [Integer] -> String
-histogram = error "Week03.Golf#histogram not implemented"
+histogram =
+  let
+    columns :: [Integer] -> [String]
+    columns = prepForOutput . map toColumn . countZeroToNine
+      where
+        countZeroToNine :: [Integer] -> [(Integer, Int)]
+        countZeroToNine xs =
+          let 
+            count :: (a -> Bool) -> [a] -> Int
+            count f = length . filter f
+          in
+            map (\x -> (x, count (== x) xs)) [0..9]
+
+        toColumn :: (Integer, Int) -> String
+        toColumn (x,y) =
+          let
+            duplicate :: Int -> String -> String
+            duplicate i = concat . replicate i
+          in
+            duplicate y "*" ++ "=" ++ show x
+
+        prepForOutput :: [String] -> [String]
+        prepForOutput xs =
+          let
+            maxLength :: [String] -> Int
+            maxLength = maximum . map length
+            padLeft :: Int -> String -> String
+            padLeft i s = replicate (i - length s) ' ' ++ s
+          in
+            map (padLeft $ maxLength xs) xs
+  in
+    unlines . transpose . columns
