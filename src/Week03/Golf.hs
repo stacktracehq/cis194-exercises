fdaa4d6274de15bd6672c1148b88e2fdda9afc8e
Fionna Song <fionna.song@stacktracehq.com>
2019-11-15 12:03:12 +1000

    week3

--- src/Week03/Golf.hs
+++ src/Week03/Golf.hs
@@ -5,11 +5,28 @@ module Week03.Golf
   )
 where
 
+
 skips :: [a] -> [[a]]
-skips = error "Week03.Golf#skips not implemented"
+skips items = map (\u -> takeEvery items u) [1..(length items)]
+  where 
+    takeEvery :: [a] -> Int -> [a]
+    takeEvery [] _ = []
+    takeEvery _ 0 = []
+    takeEvery list n = map (\u -> fst(u)) (filter (\x -> (mod (snd(x)) (n)) == 0) (zip list [1..]))
+
+
+-- localMaxima :: [Integer] -> [Integer]
+-- localMaxima [] = []
+-- localMaxima (_ : []) = []
+-- localMaxima (_ : _ : []) = []
+-- localMaxima (x : xs : xxs: xxxs) 
+--   | x < xs && xs > xxs = xs : localMaxima (xs : xxs: xxxs)
+--   |otherwise           =  localMaxima (xs : xxs: xxxs)
+
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima = error "Week03.Golf#localMaxima not implemented"
+localMaxima items = map (\(_,b,_)->b) (filter (\(x,y,z)->x<y && y>z) (zip3 items (drop 1 items) (drop 2 items)))
+
 
 histogram :: [Integer] -> String
 histogram = error "Week03.Golf#histogram not implemented"
